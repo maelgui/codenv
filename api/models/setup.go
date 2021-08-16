7c375fba2c4c23982e76af4d5894e85abf2c47c3
Mael Guillossou <mael.guillossou@gmail.com>
2021-08-16 22:09:42 +0200

    Revert to sqlite

--- api/models/setup.go
+++ api/models/setup.go
@@ -1,10 +1,9 @@
 package models
 
 import (
-	"os"
 	"time"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -18,7 +17,7 @@ type BaseModel struct {
 var DB *gorm.DB
 
 func ConnectDataBase() {
-	database, err := gorm.Open(mysql.Open(os.Getenv("DATABASE_CONF")), &gorm.Config{})
+	database, err := gorm.Open(sqlite.Open("/db/db.sqlite"), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
