61b7e486020693fdd0336a75c69e0769a793271b
Mael Guillossou <mael.guillossou@gmail.com>
2021-08-20 13:15:51 +0000

    Stop container if inactive

--- api/main.go
+++ api/main.go
@@ -5,14 +5,18 @@ import (
 	"codenv-api/docker"
 	"codenv-api/middlewares"
 	"codenv-api/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	models.ConnectDataBase()
-	docker.ConnectDocker()
+	//models.ConnectDataBase()
+	//docker.ConnectDocker()
 
 	r := gin.Default()
 
@@ -41,5 +45,40 @@ func main() {
 		ws.GET("/:taskID", controllers.AttachTerminal)
 	}
 
+	go killInactiveContainers()
+
 	r.Run()
+}
+
+type HealthzResponse struct {
+	Status        string `json:"status"`
+	LastHeartbeat string `json:"lastHeartbeat"`
+}
+
+func killInactiveContainers() {
+	for range time.Tick(time.Second * 10) {
+		var workspaces []models.Workspace
+
+		models.DB.Preload("Proxies").Find(&workspaces)
+
+		for _, w := range workspaces {
+			url, err := controllers.GetProxyRemote(w.ID, "8080")
+			if err != nil {
+				continue
+			}
+			fmt.Println("[KILLER] Checking workspace ", w.Name, " (", w.ID, ")")
+			url.Path = "/healthz"
+			r, err := http.Get(url.String())
+			if err != nil {
+				continue
+			}
+			var target HealthzResponse
+			json.NewDecoder(r.Body).Decode(&target)
+
+			if target.Status == "expired" {
+				docker.StopContainer(w.ContainerID)
+				fmt.Println("[KILLER] Stopping workspace ", w.Name, " (", w.ID, ")")
+			}
+		}
+	}
 }
